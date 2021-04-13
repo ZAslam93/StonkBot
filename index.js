84a30b00aaddef8e2d5c034d47550cfe69d37e95
Zubair Aslam <zubul93@yahoo.com>
2021-04-13 12:55:38 -0500

    Version 1

---
+++ index.js
@@ -0,0 +1,58 @@
+// Import requirements
+require("dotenv").config();
+const Discord = require("discord.js");
+const fetch = require("node-fetch");
+
+// Initialize discord client
+const client = new Discord.Client();
+
+// Call bot using $ or keyword
+let formRegex = /^\$[a-z]+/i;
+let stonkRegex = /(stonk)|(stock)/ig;
+
+// Activate bot
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+// Bot functionality
+client.on("message", msg => {
+
+  // Catch error in fetch request 
+  function errorHandle(response) {
+    if (response.ok) {
+      return response.json();
+    }
+    else {
+      msg.reply("Invalid symbol");
+      throw Error("Invalid symbol");
+    }
+    } 
+
+  // Handle symbol lookups
+  if (formRegex.test(msg.content)) {
+
+    // Parse input 
+    let quote = (msg.content).slice(1).toUpperCase();
+    console.log(quote);
+
+    // Contact API
+    fetch(`https://cloud.iexapis.com/stable/stock/${quote}/quote?token=${process.env.API}`)
+      .then(errorHandle)
+      .then(data => {
+        msg.reply(`${data["symbol"]} | ${data["companyName"]} | $${data["latestPrice"].toFixed(2)}`)
+      })
+      .catch(error => console.log(`${error} provided`));
+  }
+
+  // Handle keyword reactions
+  else if (stonkRegex.test(msg.content)) {
+    console.log(msg.content)
+    msg.react('🚀')
+      .then(console.log("TO THE MOON"))
+      .catch(console.error);
+  }
+});
+
+// Bot login
+client.login(process.env.TOKEN);

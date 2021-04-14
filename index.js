49e7e6960eaeab42a6d526246d4863136250dd95
Zubair Aslam <zubul93@yahoo.com>
2021-04-14 09:18:24 -0500

    unicode2

--- index.js
+++ index.js
@@ -40,7 +40,8 @@ client.on("message", msg => {
     fetch(`https://cloud.iexapis.com/stable/stock/${quote}/quote?token=${process.env.API}`)
       .then(errorHandle)
       .then(data => {
-        msg.reply(`${data["symbol"]} | ${data["companyName"]} | $${data["latestPrice"].toFixed(2)} ▲`)
+        data["changePercent"] > 0 ? msg.reply(`${data["symbol"]} | ${data["companyName"]} | $${data["latestPrice"].toFixed(2)} ▲`)
+          : msg.reply(`${data["symbol"]} | ${data["companyName"]} | $${data["latestPrice"].toFixed(2)} ▼`)
       })
       .catch(error => console.log(`${error} provided`));
   }
